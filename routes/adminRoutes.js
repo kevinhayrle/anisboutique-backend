ec0e2acbacad07712029d38879a69cf038e2e2ce
kevinhayrle <kevinantonyofficial@gmail.com>
2026-02-04 23:24:04 +0530

    c

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,6 +5,7 @@ const { adminLogin } = require('../controllers/adminAuthController');
 
 const {
   getAllProducts,
+  getProductById,
   deleteProduct,
   addProduct,
   updateProduct,
@@ -30,6 +31,7 @@ router.post('/login', adminLogin);
 router.get('/products', verifyToken, getAllProducts);
 router.post('/products', verifyToken, addProduct);
 router.put('/products/reorder', verifyToken, updateProductOrder);
+router.get('/products/:id', verifyToken, getProductById);
 router.put('/products/:id', verifyToken, updateProduct);
 router.delete('/products/:id', verifyToken, deleteProduct);
 
